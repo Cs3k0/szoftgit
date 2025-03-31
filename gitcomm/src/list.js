c33ff4802063b769235271bf2ebf97dc655079db
Cseko <szalontaicseko166@gmail.com>
2025-03-31 15:11:38 +0200

    htmlbase

---
+++ gitcomm/src/list.js
@@ -0,0 +1,19 @@
+export function getUnorderedList() {
+    return [
+        "Alma",
+        "Körte",
+        "Banán",
+        "Szőlő",
+        "Narancs"
+    ];
+}
+
+export function getOrderedList() {
+    return [
+        "Első elem",
+        "Második elem",
+        "Harmadik elem",
+        "Negyedik elem",
+        "Ötödik elem"
+    ];
+}
