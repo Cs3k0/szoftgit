a07db50b4a42d15579774d86ac194319c74bdb54
Cseko <szalontaicseko166@gmail.com>
2025-03-31 15:20:43 +0200

    elsoproba

--- gitcomm/src/main.js
+++ gitcomm/src/main.js
@@ -1,4 +1,27 @@
-import './style.css'
 import { getOrderedList, getUnorderedList } from './list'
 
 
+export function displayLists() {
+    const ulList = getUnorderedList();
+    const olList = getOrderedList();
+
+    const ulElement = document.getElementById("unorderedList");
+    const olElement = document.getElementById("orderedList");
+
+
+    ulElement.innerHTML = "";
+    olElement.innerHTML = "";
+
+
+    ulList.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        ulElement.appendChild(li);
+    });
+
+    olList.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        olElement.appendChild(li);
+    });
+}
